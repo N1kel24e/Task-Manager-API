9dfe6937a0ab78ce585969f339b1f3950a1e2821
N1kel24e <danyakiktenko529@gmail.com>
2026-05-24 23:07:20 +0200

    add MongoDB integration

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import  FastAPI
 from fastapi import HTTPException
 from pydantic import BaseModel
+from motor.motor_asyncio import AsyncIOMotorClient
+from bson import ObjectId
 app = FastAPI()
-tasks = []
+
+client = AsyncIOMotorClient("mongodb://localhost:27017")
+db = client.taskdb
+collection = db.tasks
 
 class Task(BaseModel):
-    id: int = 0
     title: str
     done: bool = False
 
@@ -13,36 +17,47 @@ class Task(BaseModel):
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/tasks")
-async def get_tasks():
-    return {"tasks": tasks}
-
 @app.post("/tasks")
 async def create_task(task: Task):
-    task.id = len(tasks)
-    tasks.append(task)
-    return task
+    task_dict = task.model_dump()
+    result = await collection.insert_one(task_dict)
+    task_dict["_id"] = str (result.inserted_id)
+    return task_dict
+
+@app.get("/tasks")
+async def get_tasks():
+    tasks_list = []
+    async for task in collection.find():
+        task["_id"] = str(task["_id"])
+        tasks_list.append(task)
+    return {"tasks": tasks_list}
 
 @app.get("/tasks/{task_id}")
-async def get_task(task_id: int):
-    for task in tasks:
-        if task.id == task_id:
-            return task
-    raise HTTPException(status_code=404, detail="Task not found")
+async def get_task(task_id: str):
+    task = await collection.find_one({"_id": ObjectId(task_id)})
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    task["_id"] = str(task["_id"])
+    return task
+
 
 @app.put("/tasks/{task_id}")
-async def update_task(task_id: int):
-    for task in tasks:
-        if task.id == task_id:
-            task.done = True
-            return task
-    raise HTTPException(status_code=404, detail="Task not found")
+async def update_task(task_id: str):
+    result = await collection.update_one(
+    {"_id": ObjectId(task_id)},
+    {"$set": {"done": True}}
+    )
+    task = await collection.find_one({"_id": ObjectId(task_id)})
+    if result.matched_count == 0:
+        raise HTTPException(status_code=404, detail="Task not found")
+    task["_id"] = str(task["_id"])
+    return task
 
 
 @app.delete("/tasks/{task_id}")
-async def delete_task(task_id: int):
-    for task in tasks:
-        if task.id == task_id:
-            tasks.remove(task)
-            return task
-    raise HTTPException(status_code=404, detail="Task not found")
+async def delete_task(task_id: str):
+    task = await collection.delete_one({"_id": ObjectId(task_id)})
+    if task.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return {"message": "Task deleted"}
+
